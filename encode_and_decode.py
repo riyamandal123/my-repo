c25c4cc88a16edc4613f731e2ae23c6966ceb595
riyamandal123 <96898236+riyamandal123@users.noreply.github.com>
2023-07-12 15:49:11 +0530

    Update encode_and_decode.py

--- encode_and_decode.py
+++ encode_and_decode.py
@@ -1,33 +1,40 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-
-def encrypt(plain_text,shift_count):
-    
+def caesar(start_text,shift_count,cipher_direction):
     cipher_text=""
-    for letter in plain_text:
-        position=alphabet.index(letter)
-        new_position=position + shift_count
-        new_letter=alphabet[new_position]
-        cipher_text += new_letter
-    print(f"The encode text is {cipher_text}")
+    for char in start_text:
+        if char in alphabet:
+            position=alphabet.index(char)
+            if cipher_direction == "encode":
+                new_position=position + shift_count
+                new_letter = alphabet[new_position]
+                cipher_text += new_letter
+            elif cipher_direction == "decode":
+                new_position=position - shift_count
+                new_letter = alphabet[new_position]
+                cipher_text += new_letter
+        else:
+            cipher_text += char
+    print(f"The {cipher_direction} text is {cipher_text}")
 
-#Create a function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(cipher_text,shift_count):
-    plain_text=""
-    for letter in cipher_text:
-        position=alphabet.index(letter)
-        new_position=position-shift_count
-        new_letter=alphabet[new_position]
-        plain_text += new_letter
-    print(f"The decode text is {plain_text}")
- 
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
 
-if direction == 'encode':
-    encrypt(plain_text=text,shift_count=shift)
-else:
-    decrypt(cipher_text=text,shift_count=shift)
+    caesar(start_text=text,shift_count=shift,cipher_direction=direction)
+
+    restart=input("Type 'yes' if you want to go again. Otherwise type 'no'.  ")
+    if restart == 'yes':
+        continue
+    else:
+        break
+    
+
+
+
+
+
+            
